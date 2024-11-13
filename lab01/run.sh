79a75c5b321db5b442cea7ba65b60b16d12b1d74
nguyuyen <nguyuyennguyen@gmail.com>
2024-11-13 14:19:32 +0700

    ancs

--- lab01/run.sh
+++ lab01/run.sh
@@ -2,9 +2,9 @@
 
 if [[ $1 = "compile" ]]; then
     rm -rf classes/*
-    javac -d classes/ -cp "/home/trieu/kafka/libs/*" Consumer.java Producer.java datatype/* custom/*
+    javac -d classes/ -cp "/home/nguyuyen01/kafka/libs/*" Consumer.java Producer.java datatype/* custom/*
 elif [[ $1 = "producer" ]]; then
-    java -cp "/home/trieu/kafka/libs/*:classes/" Producer
+    java -cp "/home/nguyuyen01/kafka/libs/*:classes/" Producer
 elif [[ $1 = "consumer" ]]; then
-    java -cp "/home/trieu/kafka/libs/*:classes/" Consumer $2
+    java -cp "/home/nguyuyen01/kafka/libs/*:classes/" Consumer $2
 fi
