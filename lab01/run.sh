95f3f4c5057fd00ba099acadbf3f6333eeea2a82
nguyuyen <nguyuyennguyen@gmail.com>
2024-11-14 08:57:15 +0700

    move skstream

--- lab01/run.sh
+++ lab01/run.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 = "compile" ]]; then
     rm -rf classes/*
-    javac -d classes/ -cp "/home/nguyuyen01/kafka/libs/*" Consumer.java Producer.java datatype/* custom/*
+    javac -d classes/ -cp "/home/nguyuyen01/kafka/libs/*" Consumer.java Producer.java SimpleKStream.java datatype/* custom/*
 elif [[ $1 = "producer" ]]; then
     java -cp "/home/nguyuyen01/kafka/libs/*:classes/" Producer
 elif [[ $1 = "consumer" ]]; then
